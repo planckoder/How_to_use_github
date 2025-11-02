ca876341ba40cc5f4e5a71a98dc9bb74b71628c4
planckoder <planckoder@gmail.com>
2025-11-02 11:56:10 +0100

    Some more explanations on the action "push".

--- How_to_use_github/How_to_use_github.cpp
+++ How_to_use_github/How_to_use_github.cpp
@@ -2,8 +2,9 @@
 
 int main()
 {
-/***************************************** 1.1 — Github in visualstudio 2022 ******************************************************************
+/***************************************** 1.1 — Github in visual studio 2022 ******************************************************************
 *	We can automatically save our code to github by using the integrated to VS git. First, press the window "git" just above, and choose
-*	the option "Créer un débot Git".
+*	the option "Créer un débot Git". Then, go again to the window "git", and choose the option "push", to save your project in github,
+*	because when you create a repository, it's firstly created loacaly on your computer.
 */
 }
