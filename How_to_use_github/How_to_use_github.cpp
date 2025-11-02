79fc4c4508df272571e242d83d855e709e092671
coolp <coolp@DENIR>
2025-11-02 11:44:33 +0100

    Ajoutez des fichiers projet.

---
+++ How_to_use_github/How_to_use_github.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+/***************************************** 1.1 — Github in visualstudio 2022 ******************************************************************
+*	We can automatically save our code to github by using the integrated to VS git. First, press the window "git" just above, and choose
+*	the option "Créer un débot Git".
+*/
+}
