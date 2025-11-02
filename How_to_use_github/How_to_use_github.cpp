e76f3997d30425a99372e8debe28abca60ebd6d2
Den <planckoder@gmail.com>
2025-11-02 12:14:53 +0100

    Update How_to_use_github.cpp
    Pull the code from  github.

--- How_to_use_github/How_to_use_github.cpp
+++ How_to_use_github/How_to_use_github.cpp
@@ -2,11 +2,14 @@
 
 int main()
 {
-/***************************************** 1.1 — Github in visual studio 2022 ******************************************************************
+/***************************************** 1.1 â€” Github in visual studio 2022 ******************************************************************
 *	We can automatically save our code to github by using the integrated to VS git. First, press the window "git" just above, and choose
-*	the option "Créer un débot Git". Then, go again to the window "git", and choose the option "push", to save your project in github,
+*	the option "CrÃ©er un dÃ©bot Git". Then, go again to the window "git", and choose the option "push", to save your project in github,
 *	because when you create a repository, it's firstly created loacaly on your computer. You should also use the option "Commit of 
 *	stash", because you can add a message which will explain your commitment. When you'll press the button "valider tout", be sure to 
 *	choose the option "enregistrer et valider", because to push some changes, you must first save them in your file.
+*  
+*  Also, you can write your code directly in github, if you switch to the mode editor! If you do that, commit your changes in github, 
+*  and then in visual studio, press the arrow "pull", or "tirer" in French, to recover the changes in your file.
 */
 }
